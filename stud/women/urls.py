741c61736c36655e34a640610a4861adce6e862d
VladisloveSM <mvladislav610@gmail.com>
2025-07-15 17:07:07 +0300

    Add new path with slug argument

--- stud/women/urls.py
+++ stud/women/urls.py
@@ -5,4 +5,5 @@ from women import views
 urlpatterns = [
     path('', views.index),
     path('cat/<int:cat_id>/', views.categories),
+    path('cat/<slug:cat_slug>/', views.categories_by_slug),
 ]
--- stud/women/views.py
+++ stud/women/views.py
@@ -9,3 +9,6 @@ def index(request):
 def categories(request, cat_id):
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
 
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_slug}</p>")
+
