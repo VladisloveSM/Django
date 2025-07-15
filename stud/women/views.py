d2e39d620f1a6b898194c0fcab0fd1b42a03cba5
VladisloveSM <mvladislav610@gmail.com>
2025-07-15 17:35:24 +0300

    Create converters.py, add new path to archive.

--- stud/women/views.py
+++ stud/women/views.py
@@ -12,3 +12,6 @@ def categories(request, cat_id):
 def categories_by_slug(request, cat_slug):
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_slug}</p>")
 
+def archive(request, year):
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
