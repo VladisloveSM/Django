108cdeeb974f655974cf939d45d8c041419b9fe8
VladisloveSM <mvladislav610@gmail.com>
2025-07-16 17:37:02 +0300

    Add handle of 404pageNotFoundException

--- stud/women/views.py
+++ stud/women/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
@@ -13,5 +13,10 @@ def categories_by_slug(request, cat_slug):
     return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_slug}</p>")
 
 def archive(request, year):
+    if year > 2025:
+        raise Http404
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound(f"<h1>Страница не найдена</h1>")
 
