eb2e7314096fc20de98e8fb76305f7e69e735af6
VladisloveSM <mvladislav610@gmail.com>
2025-07-21 15:35:33 +0300

    Add templates to work with html pages

--- stud/women/urls.py
+++ stud/women/urls.py
@@ -7,6 +7,7 @@ register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
     path('cat/<int:cat_id>/', views.categories, name='cats_id'),
     path('cat/<slug:cat_slug>/', views.categories_by_slug, name='cats'),
     path('archive/<year4:year>/', views.archive, name='archive'),
